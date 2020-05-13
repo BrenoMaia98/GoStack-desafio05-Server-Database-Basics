97d0b6b53a42a7220a61238706b774c390d4be5a
BrenoMaia98 <brenomaia98@gmail.com>
2020-05-12 23:14:58 -0300

    CreateTransactionService and Markdown anotation for categories and transactions tables

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,39 @@
+import {
+  Entity,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column('varchar')
   title: string;
 
+  @Column('varchar')
   type: 'income' | 'outcome';
 
+  @Column('int')
   value: number;
 
+  @Column('varchar')
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,43 @@
-// import AppError from '../errors/AppError';
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface Request {
+  category: string;
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    category,
+    title,
+    value,
+    type,
+  }: Request): Promise<Transaction> {
+    const transactionRepo = getRepository(Transaction);
+    const categoryRepo = getRepository(Category);
+
+    let categoryObject = await categoryRepo.findOne({ title: category });
+
+    if (!categoryObject) {
+      categoryObject = categoryRepo.create({ title });
+      await categoryRepo.save(categoryObject);
+    }
+
+    const newTransaction = transactionRepo.create({
+      category_id: categoryObject.id,
+      title,
+      value,
+      type,
+    });
+
+    await transactionRepo.save(newTransaction);
+
+    return newTransaction;
   }
 }
 
